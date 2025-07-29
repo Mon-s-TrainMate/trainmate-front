28a41b695c38eb2d378cadb8218add814acf26a6
hyeon <nguswjd02@gmail.com>
2025-07-29 18:44:41 +0900

    text 크기 변경

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,13 +4,13 @@ import { Search } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 const inputVariants = cva(
-  'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-[15px] py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+  'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3.75 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
   {
     variants: {
       inputSize: {
-        base: 'w-[480px] h-[68px] placeholder:text-muted-foreground placeholder:text-[18px]',
+        base: 'w-120 h-17 placeholder:text-muted-foreground placeholder:text-lg',
         search:
-          'w-[280px] h-[48px] placeholder:text-muted-foreground placeholder:text-[14px]',
+          'w-70 h-12 placeholder:text-muted-foreground placeholder:text-sm',
       },
     },
     defaultVariants: {
