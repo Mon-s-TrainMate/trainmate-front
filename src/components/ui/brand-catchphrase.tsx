8ebf6c668f4a56f111d76dfafb28ad28f9fd0956
양호진 <dahlia.jazzes0@gmail.com>
2025-07-29 17:34:02 +0900

    separate catchphrase (#8)

---
+++ src/components/ui/brand-catchphrase.tsx
@@ -0,0 +1,8 @@
+export function BrandCatchphrase() {
+  return (
+    <div className="text-[2.125rem]">
+      Let&apos;s burn{' '}
+      <span className="text-[3.375rem] text-primary">Calories!</span>
+    </div>
+  );
+}
