5189caace3a141f168f78a51daa17440462c8ce2
hyeon <nguswjd02@gmail.com>
2025-07-30 18:22:25 +0900

    라벨 크기 설정

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,21 +1,39 @@
 'use client';
 
 import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 import * as LabelPrimitive from '@radix-ui/react-label';
 
 import { cn } from '@/lib/utils';
 
+const labelVariants = cva(
+  'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+  {
+    variants: {
+      labelSize: {
+        sm: 'text-sm',
+        xs: 'text-xs',
+        base: 'text-base',
+        xl: 'text-xl',
+        xxl: 'text-2xl',
+      },
+    },
+    defaultVariants: {
+      labelSize: 'base',
+    },
+  }
+);
+
 function Label({
   className,
+  labelSize,
   ...props
-}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+}: React.ComponentProps<typeof LabelPrimitive.Root> &
+  VariantProps<typeof labelVariants>) {
   return (
     <LabelPrimitive.Root
       data-slot="label"
-      className={cn(
-        'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
-        className
-      )}
+      className={cn(labelVariants({ labelSize }), className)}
       {...props}
     />
   );
