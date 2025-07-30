197b908b54601d6d015bf4d3659c1fdf3422f7ae
hyeon <nguswjd02@gmail.com>
2025-07-30 17:47:22 +0900

    체크 박스 컴포넌트 구현

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,45 @@
+'use client';
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { CheckIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+const checkboxVariants = cva(
+  'peer border-[var(--gray-5)] dark:border-[var(--gray-5)] dark:bg-input/30 data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      checkboxSize: {
+        base: 'w-4.5 h-4.5 rounded-full',
+      },
+    },
+    defaultVariants: {
+      checkboxSize: 'base',
+    },
+  }
+);
+
+function Checkbox({
+  className,
+  checkboxSize,
+  ...props
+}: React.ComponentProps<typeof CheckboxPrimitive.Root> &
+  VariantProps<typeof checkboxVariants>) {
+  return (
+    <CheckboxPrimitive.Root
+      data-slot="checkbox"
+      className={cn(checkboxVariants({ checkboxSize }), className)}
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator
+        forceMount
+        data-slot="checkbox-indicator"
+        className="flex items-center justify-center text-[var(--gray-5)] data-[state=checked]:text-primary"
+      >
+        <CheckIcon className="size-3" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+}
+
+export { Checkbox };
