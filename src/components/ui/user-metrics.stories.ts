441a15ca00567f33db803a42fd53a2ed56cd3e3a
남현정 <97827382+nguswjd@users.noreply.github.com>
2025-07-31 14:59:21 +0900

    유저 통계 컴포넌트 (#20)

---
+++ src/components/ui/user-metrics.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import MetricsContainer from './user-metrics';
+
+const meta = {
+  title: 'ui/userMetrics',
+  component: MetricsContainer,
+} satisfies Meta<typeof MetricsContainer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const userMetrics: Story = {
+  args: {
+    trainer: 0,
+    member: 0,
+    total: 0,
+  },
+};
---
+++ src/components/ui/user-metrics.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const UserMetrics = ({
+  title = 'trainer',
+  value = 0,
+  unit = 'kcal',
+  isLast = false,
+}) => {
+  const isPlus = unit === '+';
+
+  return (
+    <section className="font-light">
+      <h2 className="text-gray-5 text-xl">{title}</h2>
+      <div className="flex items-center gap-15">
+        <p className="text-[3.25rem]">
+          {value.toLocaleString()}
+          <span
+            className={`text-gray-8 ${isPlus ? 'text-[3.25rem]' : 'text-[1.5rem] ml-2'}`}
+          >
+            {unit}
+          </span>
+        </p>
+        {!isLast && <div className="w-0.25 h-[2rem] bg-gray-2" />}
+      </div>
+    </section>
+  );
+};
+
+const MetricsContainer = ({ trainer = 0, member = 0, total = 0 }) => {
+  const metricsData = [
+    { title: 'Trainer', value: trainer, unit: '+' },
+    { title: 'Member', value: member, unit: '+' },
+    { title: 'Total', value: total, unit: 'kcal' },
+  ];
+
+  return (
+    <div className="flex gap-15">
+      {metricsData.map((metric, index) => (
+        <UserMetrics
+          key={index}
+          title={metric.title}
+          value={metric.value}
+          unit={metric.unit}
+          isLast={index === metricsData.length - 1}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MetricsContainer;
