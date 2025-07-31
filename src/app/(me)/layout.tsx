d33662acbdbe66e6c915319ed3aa2a561ee5ae7d
dahlia <dahlia.jazzes0@gmail.com>
2025-07-31 11:23:13 +0900

    group structure to prefetch /users/me

---
+++ src/app/(me)/layout.tsx
@@ -0,0 +1,29 @@
+import { getUsersMe } from '@/features/auth/actions/me';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { ReactNode } from 'react';
+import { AppSidebar } from './_app-sidebar';
+
+interface LayoutProps {
+  children?: ReactNode;
+}
+export default async function Layout({ children }: LayoutProps) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['users', 'me'],
+    queryFn: getUsersMe,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="grid grid-cols-[max-content_1fr] h-full overflow-clip">
+        <AppSidebar />
+        <main>{children}</main>
+      </div>
+    </HydrationBoundary>
+  );
+}
