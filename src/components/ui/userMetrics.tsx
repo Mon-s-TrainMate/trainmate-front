1b9eb65e620c26ea1dfd8ab79580d9deb035cb04
hyeon <nguswjd02@gmail.com>
2025-07-31 11:55:49 +0900

    전체적인 틀 잡기

--- src/components/ui/userMetrics.tsx
+++ src/components/ui/userMetrics.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
-const UserMetrics = () => {
-  return <div>유저 상태 통계</div>;
+const UserMetrics = ({
+  title = 'trainer',
+  value = 0,
+  unit = 'kcal',
+  isLast = false,
+}) => {
+  const isPlus = unit === '+';
+
+  return (
+    <div className="">
+      <h2 className="text-gray-5 text-[20px]">{title}</h2>
+      <div className="flex">
+        <p className="text-[52px]">
+          {value.toLocaleString()}
+          <span
+            className={`text-gray-8 ${isPlus ? 'text-[52px]' : 'text-[24px]'}`}
+          >
+            {unit}
+          </span>
+        </p>
+        {!isLast && <div className="w-[1px] h-[32px] bg-gray-9" />}
+      </div>
+    </div>
+  );
 };
 
 const App = () => {
-  return <UserMetrics />;
+  const metricsData = [
+    { title: 'trainer', value: 1111, unit: '+' },
+    { title: 'member', value: 2222, unit: '+' },
+    { title: 'total', value: 3333, unit: 'kcal' },
+  ];
+
+  return (
+    <div className="flex">
+      {metricsData.map((metric, index) => (
+        <UserMetrics
+          key={index}
+          title={metric.title}
+          value={metric.value}
+          unit={metric.unit}
+          isLast={index === metricsData.length - 1}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default App;
