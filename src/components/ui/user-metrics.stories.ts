da89284a33fd06144ef5e08573824fa1902c4233
hyeon <nguswjd02@gmail.com>
2025-07-31 14:34:24 +0900

    파일명 변경

--- src/components/ui/user-metrics.stories.ts
+++ src/components/ui/user-metrics.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs';
-import MetricsContainer from './userMetrics';
+import MetricsContainer from './user-metrics';
 
 const meta = {
   title: 'ui/userMetrics',
