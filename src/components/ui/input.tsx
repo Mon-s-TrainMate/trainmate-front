8cea287074a3b7e0187482fca25a534381f598b7
hyeon <nguswjd02@gmail.com>
2025-08-04 14:43:25 +0900

    type noraml, error 일때 스타일 구현

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,16 +4,22 @@ import { Search } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 const inputVariants = cva(
-  'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3.75 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+  'file:text-foreground placeholder:text-muted-foreground selection:bg-primary placeholder:px-1 selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3.75 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
   {
     variants: {
+      type: {
+        normal:
+          'hover:bg-primary-foreground focus:border-primary disabled:gray-0\.5',
+        error: 'border-red-1',
+      },
       inputSize: {
-        base: 'w-full h-17 text-lg',
-        search: 'w-full h-12 text-sm',
+        normal: 'w-full h-17 text-lg font-normal',
+        small: 'w-full h-12 text-sm font-normal',
       },
     },
     defaultVariants: {
-      inputSize: 'base',
+      type: 'normal',
+      inputSize: 'normal',
     },
   }
 );
@@ -36,7 +42,7 @@ function Input({
           type={type}
           data-slot="input"
           className={cn(
-            inputVariants({ inputSize }),
+            inputVariants({ type, inputSize }),
             'pl-10',
             'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
             className
@@ -52,7 +58,7 @@ function Input({
       type={type}
       data-slot="input"
       className={cn(
-        inputVariants({ inputSize }),
+        inputVariants({ type, inputSize }),
         'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
         className
       )}
