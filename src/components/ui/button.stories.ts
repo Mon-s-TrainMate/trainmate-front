a7322c9c7fac93c7290606eeecb91b389c60f8f5
dahlia <dahlia.jazzes0@gmail.com>
2025-08-04 17:22:32 +0900

    fix button style

--- src/components/ui/button.stories.ts
+++ src/components/ui/button.stories.ts
@@ -22,7 +22,10 @@ const meta = {
     },
     size: {
       control: 'select',
-      options: ['small', 'medium', 'large'],
+      options: ['sm', 'md', 'lg', 'icon'],
+    },
+    disabled: {
+      control: 'boolean',
     },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
@@ -33,10 +36,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const primary: Story = {
+export const Primary: Story = {
   args: {
     variant: 'primary',
-    size: 'medium',
+    size: 'md',
     children: 'Button',
   },
 };
