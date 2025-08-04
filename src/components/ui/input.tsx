d7c8eada9208b21db353b90c1968751f85559482
dahlia <dahlia.jazzes0@gmail.com>
2025-08-05 01:57:48 +0900

    fix input style

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,92 +1,30 @@
-import * as React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { Search } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import { Button } from './button';
+import * as React from 'react';
 
 const inputVariants = cva(
-  'file:text-foreground placeholder:text-muted-foreground selection:bg-primary placeholder:px-1 selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-[0.875rem] border bg-transparent px-3.75 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+  'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-[0.875rem] border bg-transparent shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-gray-5 disabled:border-gray-2 disabled:text-gray-2 hover:bg-main-5 hover:border-gray-2 focus-visible:bg-white focus-visible:border-main-2 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
   {
     variants: {
-      type: {
-        normal:
-          'hover:bg-primary-foreground focus:border-primary disabled:gray-5',
-        error: 'border-red-1',
-      },
       inputSize: {
-        normal: 'w-full h-17 text-lg font-normal',
-        small: 'w-full h-12 text-sm font-normal',
+        sm: 'h-12 px-4 text-lg rounded-sm',
+        lg: 'h-17 px-4 text-lg rounded-xl',
       },
     },
     defaultVariants: {
-      type: 'normal',
-      inputSize: 'normal',
+      inputSize: 'lg',
     },
   }
 );
 
 function Input({
   className,
-  type,
   inputSize,
-  showSearchIcon = false,
-  isButton = false,
   ...props
-}: React.ComponentProps<'input'> &
-  VariantProps<typeof inputVariants> & {
-    showSearchIcon?: boolean;
-    isButton?: boolean;
-  }) {
-  if (showSearchIcon) {
-    return (
-      <div className="relative">
-        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground pointer-events-none" />
-        <input
-          type={type}
-          data-slot="input"
-          className={cn(
-            inputVariants({ type, inputSize }),
-            'pl-10',
-            'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-            className
-          )}
-          {...props}
-        />
-      </div>
-    );
-  }
-
-  const buttonSize = {
-    normal: 'w-24.5 h-17',
-    small: 'w-24.5 h-12',
-  };
-
-  if (isButton) {
-    return (
-      <div className="flex gap-2">
-        <input
-          type={type}
-          className={cn(inputVariants({ type, inputSize }), className)}
-          {...props}
-        />
-        <Button
-          variant="primary"
-          className={buttonSize[inputSize ?? 'normal'] ?? buttonSize.normal}
-        >
-          button
-        </Button>
-      </div>
-    );
-  }
+}: React.ComponentProps<'input'> & VariantProps<typeof inputVariants>) {
   return (
     <input
-      type={type}
       data-slot="input"
-      className={cn(
-        inputVariants({ type, inputSize }),
-        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-        className
-      )}
+      className={inputVariants({ inputSize, className })}
       {...props}
     />
   );
