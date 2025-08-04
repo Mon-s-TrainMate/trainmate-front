6beffd4efa7ed079369ae8fcb70bcf85e836dd71
dahlia <dahlia.jazzes0@gmail.com>
2025-08-04 12:40:04 +0900

    add my profile-related mocks

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,11 @@
 import { mswLogin } from './handlers/api/accounts/login';
 import { mswSignup } from './handlers/api/accounts/signup';
 import { mswMemberList } from './handlers/api/members/member-list';
+import {
+  mswMyProfile,
+  mswUpdateMyProfile,
+  mswPatchMyProfile,
+} from './handlers/api/members/my-profile';
 import { mswMemberProfile } from './handlers/api/members/profile';
 import { mswMemberRecords } from './handlers/api/members/records';
 import { mswLogger } from './handlers/logger';
@@ -9,6 +14,9 @@ export const handlers = [
   mswLogger,
   mswLogin,
   mswSignup,
+  mswMyProfile,
+  mswUpdateMyProfile,
+  mswPatchMyProfile,
   mswMemberProfile,
   mswMemberList,
   mswMemberRecords,
