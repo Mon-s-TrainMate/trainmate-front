ebf6aea5fc744fbf8690087a2a452e45bbf46ada
dahlia <dahlia.jazzes0@gmail.com>
2025-08-06 12:30:05 +0900

    add member profile hook

---
+++ src/features/member/hooks/use-member-profile.ts
@@ -0,0 +1,10 @@
+import { getMemberProfileQueryKey } from '@/lib/users/query-key';
+import { useQuery } from '@tanstack/react-query';
+import { getMemberProfileById } from '../api/get-member-profile-by-id';
+
+export function useMemberProfile(memberId: string) {
+  return useQuery({
+    queryKey: getMemberProfileQueryKey(memberId),
+    queryFn: ({ queryKey }) => getMemberProfileById(queryKey[1]),
+  });
+}
