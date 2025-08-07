696b460971b05a8cc4dc040cbbd0f8eab93a86d4
남현정 <97827382+nguswjd@users.noreply.github.com>
2025-08-07 20:32:07 +0900

    Login 스타일 수정 (#54)

--- src/app/(me)/auth/sign-in/page.tsx
+++ src/app/(me)/auth/sign-in/page.tsx
@@ -2,6 +2,7 @@
 
 import { BrandCatchphrase } from '@/components/ui/brand-catchphrase';
 import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
 import {
   Form,
   FormControl,
@@ -20,23 +21,15 @@ import {
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
 import { Input } from '@/components/ui/input';
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Separator } from '@/components/ui/separator';
 import { getUsersQueryKey } from '@/lib/users/query-key';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { useQueryClient } from '@tanstack/react-query';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { getUser } from './_actions/action';
-import { SignInFormSchema, signInFormSchema } from './schema';
 import { AlertCircle } from 'lucide-react';
 import { useState } from 'react';
-
-const userTypes: { value: SignInFormSchema['userType']; label: string }[] = [
-  { value: 'member', label: '개인 회원' },
-  { value: 'trainer', label: '트레이너' },
-];
 
 export default function Page() {
   const router = useRouter();
@@ -44,7 +37,6 @@ export default function Page() {
   const [isAlertOpen, setIsAlertOpen] = useState(false);
 
   const form = useForm({
-    resolver: zodResolver(signInFormSchema),
     defaultValues: {
       userType: 'member' as const,
       email: '',
@@ -53,142 +45,105 @@ export default function Page() {
     },
   });
   return (
-    <div className="mt-35 flex flex-col items-center gap-15">
-      <BrandCatchphrase />
-      <Form {...form}>
-        <form
-          className="flex w-full max-w-lg flex-col gap-y-10 px-4"
-          onSubmit={form.handleSubmit(async (values) => {
-            const res = await getUser(values);
-            if (res.success) {
-              await queryClient.invalidateQueries({
-                queryKey: getUsersQueryKey('me'),
-              });
-              router.push('/');
-            } else {
-              for (const [key, errors] of Object.entries(res.errors)) {
-                const fieldName = key === 'non_field_errors' ? 'root' : key;
-                form.setError(fieldName as 'root', {
-                  type: 'manual',
-                  message: errors[0],
+    <div className="mt-35 flex flex-col items-center">
+      <div className="flex w-136 flex-col items-center gap-15 rounded-xl bg-white p-8">
+        <BrandCatchphrase />
+        <Form {...form}>
+          <form
+            className="flex w-full max-w-lg flex-col gap-y-10 px-4"
+            onSubmit={form.handleSubmit(async (values) => {
+              const res = await getUser(values);
+              if (res.success) {
+                await queryClient.invalidateQueries({
+                  queryKey: getUsersQueryKey('me'),
                 });
+                router.push('/');
+              } else {
+                for (const [key, errors] of Object.entries(res.errors)) {
+                  const fieldName = key === 'non_field_errors' ? 'root' : key;
+                  form.setError(fieldName as 'root', {
+                    type: 'manual',
+                    message: errors[0],
+                  });
+                }
+                setIsAlertOpen(true);
               }
-              setIsAlertOpen(true);
-            }
-          })}
-        >
-          <FormField
-            control={form.control}
-            name="userType"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="sr-only">회원 유형</FormLabel>
-                <FormControl>
-                  <RadioGroup
-                    className="grid grid-cols-2 gap-2.5 rounded-xl bg-gray-4 p-2"
-                    onValueChange={field.onChange}
-                    value={field.value}
-                  >
-                    {userTypes.map((userType) => (
-                      <FormItem key={userType.value}>
-                        <FormControl>
-                          <RadioGroupItem
-                            className="peer hidden"
-                            value={userType.value}
-                          />
-                        </FormControl>
-                        <FormLabel className="flex h-13 items-center justify-center rounded-md bg-transparent text-center text-lg font-bold text-gray-2 peer-aria-checked:bg-white peer-aria-checked:text-primary peer-aria-checked:shadow-tab-active">
-                          {userType.label}
-                        </FormLabel>
-                      </FormItem>
-                    ))}
-                  </RadioGroup>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>이메일</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="이메일 주소를 입력해주세요."
-                    {...field}
-                    className={`focus-visible:border-primary ${
-                      field.value ? 'border-primary' : ''
-                    }`}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>비밀번호</FormLabel>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="비밀번호를 입력해주세요."
-                    {...field}
-                    className={`focus-visible:border-primary ${
-                      field.value ? 'border-primary' : ''
-                    }`}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {form.formState.errors.root && (
-            <p className="text-sm text-destructive">
-              {form.formState.errors.root.message}
-            </p>
-          )}
-          <FormField
-            control={form.control}
-            name="keepLogin"
-            render={({ field }) => (
-              <FormItem>
-                <Separator />
-                <div className="flex items-center">
+            })}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>이메일</FormLabel>
                   <FormControl>
-                    <input
-                      type="checkbox"
-                      ref={field.ref}
-                      name={field.name}
-                      checked={field.value}
-                      onChange={field.onChange}
-                      disabled={field.disabled}
-                      onBlur={field.onBlur}
+                    <Input
+                      placeholder="이메일 주소를 입력해주세요."
+                      {...field}
+                      className={`focus-visible:border-primary ${
+                        field.value ? 'border-primary' : ''
+                      }`}
                     />
                   </FormControl>
-                  <FormLabel className={field.value ? 'text-primary' : ''}>
-                    로그인 상태 유지하기
-                  </FormLabel>
-                </div>
-                <FormMessage />
-              </FormItem>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>비밀번호</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="비밀번호를 입력해주세요."
+                      {...field}
+                      className={`focus-visible:border-primary ${
+                        field.value ? 'border-primary' : ''
+                      }`}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {form.formState.errors.root && (
+              <p className="text-sm text-destructive">
+                {form.formState.errors.root.message}
+              </p>
             )}
-          ></FormField>
-          <Button type="submit">로그인하기</Button>
-          <Link
-            href="/auth/sign-up"
-            className="grid grid-cols-[1fr_max-content_1fr] items-center gap-x-6.25 break-keep text-gray-2"
-          >
-            <Separator />
-            <span>회원가입</span>
-            <Separator />
-          </Link>
-        </form>
-      </Form>
+            <FormField
+              control={form.control}
+              name="keepLogin"
+              render={({ field }) => (
+                <FormItem>
+                  <Separator />
+                  <div className="flex items-center gap-2">
+                    <FormControl>
+                      <Checkbox onCheckedChange={field.onChange} />
+                    </FormControl>
+                    <FormLabel className={field.value ? 'text-primary' : ''}>
+                      로그인 상태 유지하기
+                    </FormLabel>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <Button type="submit">로그인하기</Button>
+            <Link
+              href="/auth/sign-up"
+              className="grid grid-cols-[1fr_max-content_1fr] items-center gap-x-6.25 break-keep text-gray-2"
+            >
+              <Separator />
+              <span>회원가입</span>
+              <Separator />
+            </Link>
+          </form>
+        </Form>
+      </div>
 
       <AlertDialog open={isAlertOpen} onOpenChange={setIsAlertOpen}>
         <AlertDialogContent>
