6d42eecc597fb2c14bab03bf358e291ae4aa39d9
hyeon <nguswjd02@gmail.com>
2025-08-12 15:53:57 +0900

    div태그 section으로 변경

--- src/app/(me)/(with-header)/(with-records)/_ui/sidebar.tsx
+++ src/app/(me)/(with-header)/(with-records)/_ui/sidebar.tsx
@@ -15,24 +15,27 @@ export function Sidebar() {
         </Link>
       </header>
       <div className="flex flex-col gap-3 p-6">
-        <div className="flex h-30 items-center justify-center text-center">
+        <section className="flex h-30 items-center justify-center text-center">
+          <h2 className="sr-only">저장된 운동</h2>
           <p className="self-center text-sm text-gray-2">
             등록된 운동이 없습니다.
             <br />
             새로운 운동을 등록해보세요.
           </p>
-        </div>
-        <div className="flex justify-between text-base font-light text-gray-2">
+        </section>
+        <section className="flex justify-between text-base font-light text-gray-2">
+          <h2 className="sr-only">총 운동 시간 및 칼로리</h2>
           <p className="flex items-center gap-4">
-            time <span className="text-2xl text-main-2">00:00:00</span>
+            time <span className="text-2xl text-main-2">0000</span>
           </p>
           <p className="flex items-center gap-4 font-light">
             total{' '}
-            <p className="text-2xl text-main-2">
-              16,510 <span className="text-base text-gray-1">kcal</span>
-            </p>
+            <span className="text-2xl text-main-2">
+              0000
+              <span className="text-base text-gray-1"> kcal</span>
+            </span>
           </p>
-        </div>
+        </section>
       </div>
       <div className="pt-3 pr-6 pb-3 pl-6">
         <Button>저장하기</Button>
