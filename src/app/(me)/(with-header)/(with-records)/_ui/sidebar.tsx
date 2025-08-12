ed353d1df93e175cf99bdda267309bb86ea8eaf2
hyeon <nguswjd02@gmail.com>
2025-08-12 14:20:26 +0900

    등록 운동 안내 문구추가

--- src/app/(me)/(with-header)/(with-records)/_ui/sidebar.tsx
+++ src/app/(me)/(with-header)/(with-records)/_ui/sidebar.tsx
@@ -3,6 +3,7 @@
 import { ChevronLeftIcon } from 'lucide-react';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
+import { Button } from '@/components/ui/button';
 
 export function Sidebar() {
   const { memberId } = useParams();
@@ -13,6 +14,16 @@ export function Sidebar() {
           <ChevronLeftIcon />
         </Link>
       </header>
+      <div className="flex h-30 items-center justify-center p-6 text-center">
+        <p className="text-sm text-gray-2">
+          등록된 운동이 없습니다.
+          <br />
+          새로운 운동을 등록해보세요.
+        </p>
+      </div>
+      <div className="pt-3 pr-6 pb-3 pl-6">
+        <Button className="">저장하기</Button>
+      </div>
     </aside>
   );
 }
