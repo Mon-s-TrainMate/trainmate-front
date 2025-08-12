d305798ac670f28deb00c2a7f557026cd0ff47ca
dahlia <dahlia.jazzes0@gmail.com>
2025-08-12 17:17:32 +0900

    add new workout link

--- src/app/(me)/(with-header)/(with-members)/members/[memberId]/_ui/member-record-widget.tsx
+++ src/app/(me)/(with-header)/(with-members)/members/[memberId]/_ui/member-record-widget.tsx
@@ -65,9 +65,11 @@ export function MemberRecordWidget({ memberId }: MemberRecordWidgetProps) {
         ))}
       </div>
       <div className="flex flex-col items-center justify-center py-6 @lg:px-6">
-        <Button className="w-full @lg:max-w-80">
-          운동 관리하기
-          <ChevronRightIcon />
+        <Button className="w-full @lg:max-w-80" asChild>
+          <Link href={`/members/${memberId}/records/new`}>
+            운동 관리하기
+            <ChevronRightIcon />
+          </Link>
         </Button>
       </div>
     </div>
