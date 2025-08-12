925f7bfa44b4e8cd3527b4754600e10f73cc91c7
hyeon <nguswjd02@gmail.com>
2025-08-12 15:05:33 +0900

    total time/kcal 추가

--- src/app/(me)/(with-header)/(with-records)/_ui/sidebar.tsx
+++ src/app/(me)/(with-header)/(with-records)/_ui/sidebar.tsx
@@ -14,15 +14,28 @@ export function Sidebar() {
           <ChevronLeftIcon />
         </Link>
       </header>
-      <div className="flex h-30 items-center justify-center p-6 text-center">
-        <p className="text-sm text-gray-2">
-          등록된 운동이 없습니다.
-          <br />
-          새로운 운동을 등록해보세요.
-        </p>
+      <div className="flex flex-col gap-3 p-6">
+        <div className="flex h-30 items-center justify-center text-center">
+          <p className="self-center text-sm text-gray-2">
+            등록된 운동이 없습니다.
+            <br />
+            새로운 운동을 등록해보세요.
+          </p>
+        </div>
+        <div className="flex justify-between text-base font-light text-gray-2">
+          <p className="flex items-center gap-4">
+            time <span className="text-2xl text-main-2">00:00:00</span>
+          </p>
+          <p className="flex items-center gap-4 font-light">
+            total{' '}
+            <p className="text-2xl text-main-2">
+              16,510 <span className="text-base text-gray-1">kcal</span>
+            </p>
+          </p>
+        </div>
       </div>
       <div className="pt-3 pr-6 pb-3 pl-6">
-        <Button className="">저장하기</Button>
+        <Button>저장하기</Button>
       </div>
     </aside>
   );
