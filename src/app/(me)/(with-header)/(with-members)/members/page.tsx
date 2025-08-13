db15d55091ee1168f91f0a0f0dc2d3aa79b7fcbc
dahlia <dahlia.jazzes0@gmail.com>
2025-08-13 12:18:31 +0900

    move all auth-guard logic to client

--- src/app/(me)/(with-header)/(with-members)/members/page.tsx
+++ src/app/(me)/(with-header)/(with-members)/members/page.tsx
@@ -1,18 +1,19 @@
-import { authGuard } from '@/features/auth/actions/auth-guard';
+import { AuthGuard } from '@/features/auth/ui/auth-guard';
 import { UserAvatar } from '@/features/user/ui/user-avatar';
 
 export default async function Page() {
-  await authGuard();
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex items-center gap-x-5">
-        <UserAvatar size="lg" />
-        <p className="text-2xl text-gray-1">
-          회원님을 선택하고
-          <br />
-          운동을 시작해 볼까요?
-        </p>
+    <AuthGuard>
+      <div className="flex items-center justify-center">
+        <div className="flex items-center gap-x-5">
+          <UserAvatar size="lg" />
+          <p className="text-2xl text-gray-1">
+            회원님을 선택하고
+            <br />
+            운동을 시작해 볼까요?
+          </p>
+        </div>
       </div>
-    </div>
+    </AuthGuard>
   );
 }
--- src/features/auth/actions/auth-guard.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getUsersMe } from './me';
-
-export async function authGuard() {
-  const me = await getUsersMe();
-  if (me == null) redirect('/auth/sign-in');
-  return me;
-}
