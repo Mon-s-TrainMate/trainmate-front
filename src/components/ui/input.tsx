d64084c7dcf95f304b4b4ef82696faf0de198aeb
dahlia <dahlia.jazzes0@gmail.com>
2025-08-14 12:33:06 +0900

    fix input background

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,7 +2,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
 const inputVariants = cva(
-  'shadow-xs flex h-9 w-full min-w-0 rounded-[0.875rem] border border-input bg-transparent transition-[color,box-shadow] outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground hover:border-gray-2 hover:bg-main-5 focus-visible:border-main-2 focus-visible:bg-white disabled:pointer-events-none disabled:cursor-not-allowed disabled:border-gray-2 disabled:bg-gray-5 disabled:text-gray-2 aria-invalid:border-red-1 aria-invalid:ring-destructive/20 dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
+  'shadow-xs flex h-9 w-full min-w-0 rounded-[0.875rem] border border-input bg-white transition-[color,box-shadow] outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground hover:border-gray-2 hover:bg-main-5 focus-visible:border-main-2 focus-visible:bg-white disabled:pointer-events-none disabled:cursor-not-allowed disabled:border-gray-2 disabled:bg-gray-5 disabled:text-gray-2 aria-invalid:border-red-1 aria-invalid:ring-destructive/20 dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
   {
     variants: {
       inputSize: {
