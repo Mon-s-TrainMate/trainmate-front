c9da3b3a1353720949d2c125cedfc38d62c7d4a7
hyeon <nguswjd02@gmail.com>
2025-08-14 14:40:51 +0900

    백그라운드 이미지 추가

--- src/app/(me)/(with-header)/page.tsx
+++ src/app/(me)/(with-header)/page.tsx
@@ -1,9 +1,12 @@
 import MetricsContainer from '../../../components/ui/user-metrics';
+import Image from 'next/image';
+import background from '../../../../public/background-img.png';
 
 export default function Home() {
   return (
     <div>
       <MetricsContainer></MetricsContainer>
+      <Image src={background} alt="" />
     </div>
   );
 }
