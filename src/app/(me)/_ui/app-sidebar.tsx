c1239386c4b3b4436b358ece7eabd66dd7ab57a5
남현정 <97827382+nguswjd@users.noreply.github.com>
2025-08-15 19:45:19 +0900

    Logout layout (#92)
    Co-authored-by: dahlia <dahlia.jazzes0@gmail.com>

--- src/app/(me)/_ui/app-sidebar.tsx
+++ src/app/(me)/_ui/app-sidebar.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Separator } from '@/components/ui/separator';
 import { useMyProfile } from '@/features/member/hooks/use-my-profile';
 import { UserAvatar } from '@/features/user/ui/user-avatar';
 import {
@@ -10,27 +17,18 @@ import {
 } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { ElementType } from 'react';
+import { ElementType, useState } from 'react';
 
-// Menu items.
 const items = [
-  {
-    id: 0,
-    href: '/members',
-    Icon: UserRoundIcon,
-  },
-  {
-    id: 1,
-    Icon: CalendarIcon,
-  },
-  {
-    id: 2,
-    Icon: MessageSquareMoreIcon,
-  },
+  { id: 0, href: '/members', Icon: UserRoundIcon },
+  { id: 1, Icon: CalendarIcon },
+  { id: 2, Icon: MessageSquareMoreIcon },
 ];
 
 export function AppSidebar() {
   const { data: user } = useMyProfile();
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+
   return (
     <aside className="sticky top-0 z-50 grid w-22 grid-rows-[max-content_1fr_max-content] overflow-clip bg-white shadow-sidebar">
       <header className="flex flex-col items-center gap-y-4 px-4 pt-6">
@@ -43,13 +41,48 @@ export function AppSidebar() {
           className="h-0.5 w-10 rounded-full bg-gray-4"
         ></div>
       </header>
+
       <section className="flex flex-col gap-y-2 py-8.25">
         {items.map((item) => (
           <NavLink key={item.id} {...item} />
         ))}
       </section>
-      <footer className="pb-3">
-        <NavLink href="/settings" Icon={SettingsIcon} />
+
+      <footer className="relative mb-3 flex justify-center">
+        <Popover open={isSettingsOpen} onOpenChange={setIsSettingsOpen}>
+          <PopoverTrigger
+            className={`flex size-12 items-center justify-center rounded-full hover:border-main-5 hover:bg-main-5 ${
+              isSettingsOpen ? 'bg-main-5 text-main-2' : 'bg-white text-black'
+            }`}
+            data-active={isSettingsOpen}
+            onClick={() => setIsSettingsOpen(!isSettingsOpen)}
+          >
+            <SettingsIcon className="strokeWidth={1} size-6" />
+            {isSettingsOpen && (
+              <span className="absolute top-1/2 right-full size-2 translate-x-1/2 -translate-y-1/2 rounded-full bg-primary group-data-[active=true]:block" />
+            )}
+          </PopoverTrigger>
+          <PopoverContent
+            side="right"
+            sideOffset={36}
+            align="start"
+            collisionPadding={24}
+            className="flex flex-col gap-4 rounded-lg p-4"
+          >
+            <div className="flex flex-col items-center gap-3">
+              <UserAvatar />
+              <p className="font-semibold text-main-2">버전정보: v0.0.1</p>
+            </div>
+            <Separator />
+            <Button
+              variant="text"
+              className="underline"
+              onClick={() => setIsSettingsOpen(false)}
+            >
+              로그아웃
+            </Button>
+          </PopoverContent>
+        </Popover>
       </footer>
     </aside>
   );
@@ -59,9 +92,11 @@ interface NavLinkProps {
   href?: string;
   Icon: ElementType;
 }
+
 function NavLink({ href, Icon }: NavLinkProps) {
   const pathname = usePathname();
-  if (href == null) {
+
+  if (!href) {
     return (
       <button className="flex appearance-none justify-center">
         <div className="flex size-12 items-center justify-center rounded-full text-gray-2">
